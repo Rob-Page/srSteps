52586fdcea94caf822ca6cbc1794177a2d48616c
Rob-Page <pager925@gmail.com>
2017-10-12 20:20:28 -0600

    post and get routes test

--- controllers/apiRoutes.js
+++ controllers/apiRoutes.js
@@ -8,46 +8,47 @@ var sr_object = require("../models/sr.js");
 
 // get an sr object
 router.get("/api/:sr_number", function(req, res) {
-	// GET SR object based on the sr_number parameters 
-	sr_object.sr(req.params.sr_number, function(result) {
-		// callback responds with a JSON object that is passed in from the model and orm files
-		res.json(result);
-	});
+    // GET SR object based on the sr_number parameters 
+    sr_object.sr(req.params.sr_number, function(result) {
+        // callback responds with a JSON object that is passed in from the model and orm files
+        res.json(result);
+    });
 });
 
 // get a step from an sr
 router.get("/api/:sr_number/:step", function(req, res) {
-	// get a step attribute based on the sr params and the step params 
-	sr_object.step(req.params.sr_number, req.params.step, function(result) {
-		// callback responds with a JSON object that is passed in from the model and orm files
-		res.json(result);
-	});
+    // get a step attribute based on the sr params and the step params 
+    sr_object.step(req.params.sr_number, req.params.step, function(result) {
+        // callback responds with a JSON object that is passed in from the model and orm files
+        res.json(result);
+    });
 });
 
 router.post("/api/:sr_number", function(req, res) {
-	// GRAPS THE sr NUMBER from the url
-	// gets the data from the body as a JSON OBJECT
-	// calls the new function and passes in the call back and the sr number with data
-	sr_object.new(req.params.sr_number, req.body, function(result) {
-		console.log();
-	});
+    //TODO: integrate this logic into the model rather than the controller
+    //logic check for data consistence between url sr and data sr
+    if (req.params.sr_number == req.body.sr_num) {
+        // GRAPS THE sr NUMBER from the url
+        // gets the data from the body as a JSON OBJECT
+        // calls the new function and passes in the call back and the sr number with data
 
-//TODO: integrate this logic into the model rather than the controller
-	//logic check for data consistence between url sr and data sr
-	if(req.params.sr_number == req.body.sr_num){
-		// if the logic is true return success
-		res.end("Success")
-	}
-	else{
-		// if the logic was not true tell the post origin the following message
-		res.end('ERROR:' + 'The sr number passed in through the url did not match the data');
-	}
-	
+        sr_object.new(req.params.sr_number, req.body, function(result) {
+            res.end("Success sr object added")
+        });
+        // if the logic is true return success
+
+    } else {
+        // if the logic was not true tell the post origin the following message
+        res.end('ERROR:' + 'The sr number passed in through the url did not match the data');
+    }
+
 });
 
 router.post("/api/:sr_number/:step", function(req, res) {
-	// fs.writeFile('data/test.txt', JSON.stringify(hbsObject), function(err) {})
-	console.log(req.body);
+    sr_object.update(req.params.sr_number, req.params.sr_number, req.body, function(result) {
+        res.end("Success step added")
+    });
+    console.log(req.body);
 });
 // Export routes for server.js to use.
 module.exports = router;
--- public/js/script.js
+++ public/js/script.js
@@ -1,12 +1,40 @@
-document.querySelector("#test").onclick  = function(){
+document.querySelector("#getTest").onclick  = function(){
 
+// gets sr attribute passed in in the url
+httpGetAsync("/api/33333/IC", function(response){
+	console.log(response);
+});
+
+// gets sr by number
 httpGetAsync("/api/33333", function(response){
 	console.log(response);
 });
 
+};		
+
+
+document.querySelector("#postTest").onclick  = function(){
+var body1 = "this is the IC tag";
+
+
+var body2 ={
+        "sr_num": "33333",
+        "IC": "test this stuff for what IO want",
+        "IV": "test this stuff for what IO want",
+        "CD": "test this stuff for what IO want",
+        "CV": "test this stuff for what IO want",
+        "PS": "test this stuff for what IO want",
+        "SAP": "test this stuff for what IO want"
+    };
+// gets sr by number
+httpPostAsyncJSON("/api/33333", body2 , function(response){
+	console.log(response);
+});
+
+httpPostAsync("/api/33333/IC", body1 , function(response){
+	console.log(response);
+});
 };
-
-
 
 function httpGetAsync(theUrl, callback)
 {
@@ -17,6 +45,31 @@ function httpGetAsync(theUrl, callback)
     }
     xmlHttp.open("GET", theUrl, true); // true for asynchronous 
     xmlHttp.send(null);
+}
+
+function httpPostAsync(theUrl, body, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    
+    xmlHttp.open("POST", theUrl, true); // true for asynchronous 
+    xmlHttp.send(JSON.stringify(body));
+}
+
+function httpPostAsyncJSON(theUrl, body, callback)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+            callback(xmlHttp.responseText);
+    }
+    
+    xmlHttp.open("POST", theUrl, true); // true for asynchronous 
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(JSON.stringify(body));
 }
 
 // var http = new XMLHttpRequest();
